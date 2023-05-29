3d0836982ea1782cc259d6d672a9f09fc1ccf364
Wendy Liu <yunyiwendy.liu@gmail.com>
2023-05-29 00:41:29 -0400

    Added user table migration + Finished user/register endpoint

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class UserController extends BaseController
@@ -11,6 +13,25 @@ class UserController extends BaseController
     {
         $email = $request->input('email');
         $password = $request->input('password');
+
+        try {
+            // First, check if the email already exists
+            $userObj = DB::table('user')
+                ->where('email', '=', $email)
+                ->first();
+            if ($userObj) {
+                return response()->json("User $email already exists", 400);
+            }
+
+            // Insert email to user table
+            DB::table('user')
+                ->insert([
+                    'email' => $email,
+                    'password' => $password
+                ]);
+        } catch (QueryException $e) {
+            return response()->json(['error' => $e->getMessage()], 400);
+        }
 
         return response()->json([
             'email' => $email,

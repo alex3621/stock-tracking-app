961707b567990ca87bcb737c903cc4744230a180
Alex Lin <chinyinalex.lin@gmail.com>
2023-09-03 22:14:47 -0400

    added 3 routes for group stock and stockController

---
+++ server/app/Http/Controllers/StockController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class StockController extends BaseController
+{
+    public function manage(Request $request)
+    {
+    }
+
+    public function stockList(Request $request)
+    {
+    }
+}
--- server/routes/web.php
+++ server/routes/web.php
@@ -16,12 +16,23 @@
 //calling API example (localhost:user/functionName)
 //$router->post/get('endpoint name', 'controllerFileName@functionName')
 $router->group(['prefix' => 'user'], function () use ($router) {
+    //user can register for an account here
     $router->post('register', 'UserController@register');
-    // TODO: Include other user endpoints
+
+    // login page
     $router->post('login', 'UserController@login');
 });
 
 
 $router->group(['prefix' => 'stock'], function () use ($router) {
     // TODO: Include other stock endpoints here
+
+    // Main page when logged in, displays stocks user currently wants to keep track
+    $router->post('dashboard', 'StockController@dashboard');
+
+    // Page for managing stocks, adding and removing
+    $router->post('manage', 'StockController@manage');
+
+    // List of stocks that can be sorted 
+    $router->post('stockList', 'StockController@stockList');
 });
